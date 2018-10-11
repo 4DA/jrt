be3f247d26a8d5d59d393ae9c3c983795cf6996f
Dmitry Cherkassov <dcherkassov@gmail.com>
2018-10-12 02:08:43 +0300

    world

--- rt1.jl
+++ rt1.jl
@@ -8,12 +8,12 @@ struct Ray
     direction::Array{Float64, 1}
 end
 
-struct hit_record
+struct HitRecord
     t::Float64
     p::Array{Float64}
     normal::Array{Float64}
-    hit_record() = new(0.0, [0.0, 0.0, 0.0], [0.0, 0.0, 0.0])
-    hit_record(t,p,normal) = new(t, p, normal)
+    HitRecord() = new(0.0, [0.0, 0.0, 0.0], [0.0, 0.0, 0.0])
+    HitRecord(t,p,normal) = new(t, p, normal)
 end
 
 function point_at_parameter(r::Ray, t::Float64)
@@ -25,7 +25,7 @@ struct Sphere <: Hitable
     radius::Float64
 end
 
-function hit(sphere::Sphere, r::Ray, t_min::Float64, t_max::Float64)::Union{hit_record, Nothing}
+function hit(sphere::Sphere, r::Ray, t_min::Float64, t_max::Float64)::Union{HitRecord, Nothing}
     a = dot(r.direction, r.direction)
     s2o = r.origin - sphere.center
     b = 2.0 * dot(r.direction, s2o)
@@ -35,7 +35,7 @@ function hit(sphere::Sphere, r::Ray, t_min::Float64, t_max::Float64)::Union{hit_
         t1 = (-b - sqrt(D)) / (2.0 * a)
         if (t1 < t_max && t1 > t_min)
             p = point_at_parameter(r, t1)
-            return hit_record(t1,
+            return HitRecord(t1,
                               p,
                               (p - sphere.center) / sphere.radius)
         end
@@ -43,19 +43,32 @@ function hit(sphere::Sphere, r::Ray, t_min::Float64, t_max::Float64)::Union{hit_
         t2 = (-b + sqrt(D)) / (2.0 * a)
         if (t2 < t_max && t2 > t_min)
             p = point_at_parameter(r, t1)
-            return hit_record(t2,
+            return HitRecord(t2,
                               p, 
                               (p - sphere.center) / sphere.radius)
         end
-        return Nothing
+        return nothing
     end
 end
 
-function color(r::Ray)
-    sphere_center = [0.0, 0.0, -1.0]
-    sphere_radius = 0.5
-    hitres = hit(Sphere(sphere_center, sphere_radius), r, 0.0, 10000.0)
-    if (isa(hitres, hit_record))
+function hit(hitables::Array{Hitable}, r::Ray, t_min::Float64, t_max::Float64)::Union{HitRecord, Nothing}
+    result::Union{HitRecord, Nothing} = nothing
+    closest_t = t_max
+
+    for i = 1:length(hitables)
+        hit_res = hit(hitables[i], r, t_min, closest_t)
+        if (isa(hit_res, HitRecord))
+            closest_t = hit_res.t
+            result = hit_res
+        end
+    end
+
+    return result
+end
+
+function color(r::Ray, world::Array{Hitable})
+    hitres = hit(world, r, 0.0, 10000.0)
+    if (isa(hitres, HitRecord))
         return 0.5 * (hitres.normal + [1.0, 1.0, 1.0])
     else
         u = r.direction / norm(r.direction)
@@ -74,13 +87,15 @@ function main()
     vertical = [0.0, 2.0, 0.0]
     origin = [0.0, 0.0, 0.0]
 
+    world::Array{Hitable} = [Sphere([0.0, 0.0, -1.0], 0.5), Sphere([0.0, -100.5, -1], 100)]
+
     for j::Int = ny - 1 : -1 : 0
         for i::Int = 0 : nx - 1
             u::Float64 = convert(Float64, i) / nx
             v::Float64 = convert(Float64, j) / ny
 
             r = Ray(origin, lower_left_corner + u * horizontal + v * vertical)
-            col = color(r)
+            col = color(r, world)
                     
             ir::Int = trunc(255.99 * col[1])
             ig::Int = trunc(255.99 * col[2])
