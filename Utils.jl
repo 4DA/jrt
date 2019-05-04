32a3d5413299381e4936ff9366f694f9929a0f44
Dmitry Cherkassov <dcherkassov@gmail.com>
2019-05-04 20:08:29 +0300

    comment on random cosine direction

--- Utils.jl
+++ Utils.jl
@@ -28,9 +28,35 @@ end
 
 # This will always converge to the right answer. The more ​ p ​ follows f, the faster it converges.
 
-# p(dir) = cos(theta) / pi
-# r2 = Integral[0, theta, 2 * pi * (cos(t) / pi) * sin(t)] = 1 - cos^2(theta)
-# so cos(theta) = sqrt(1-r2)
+# some theory on this:
+# https://www.scratchapixel.com/lessons/3d-basic-rendering/global-illumination-path-tracing/global-illumination-path-tracing-practical-implementation
+
+# sample space = 2PI steradians
+# Int[p(ω) dω, {ω, 0, 2PI}] = 1
+# p(ω) = C = const, bc all samples have the same chance
+# p(ω) = C = 1 / 2PI | by evaluating the integral
+# dω = sinθ * dθ * dφ | differential solid angle
+# p(φ, θ) * dω * dφ = p(ω) * dω
+# p(φ, θ) = sinθ / 2PI | join probability distribution.
+
+# A marginal distribution is one in which the probability of a given variable
+# (for example θ) can be expressed without any reference to the other variables
+# (for example ϕ). You can do this by integrating the given PDF with respect to
+# one of the variables. In our case, we get (will integrate p(θ,ϕ) with respect
+# to ϕ):
+
+# P(θ) = Int[p(φ, θ) dφ, {φ, 0, 2PI}] = sinθ
+
+# The conditional probability function gives the probably function of one of the
+# variables (for example ϕ) when the other variable (θ) is known. For continuous
+# function (which the case of our PDFs in this example) this can simply be
+# computed as the joint probability distribution p(θ,ϕ) over the marginal
+# distribution p(θ):
+# p(φ) = p(θ, φ) / p(θ) = 1 / 2PI
+
+# Now that we have the PDFs for θ and ϕ, we need to compute their respective
+# CDFs and inverse them.
+
 function random_cosine_direction()::Vec3
     r1 = rand()
     r2 = rand()
